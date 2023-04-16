24e8e882f08a0d7cf5a5d472019bd82fcf14ac1d
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-16 18:14:42 +0530

    Update 11-indexed-access-with-unions.problem.ts

--- src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
+++ src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
@@ -1,21 +1,24 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 export const programModeEnumMap = {
-  GROUP: "group",
-  ANNOUNCEMENT: "announcement",
-  ONE_ON_ONE: "1on1",
-  SELF_DIRECTED: "selfDirected",
-  PLANNED_ONE_ON_ONE: "planned1on1",
-  PLANNED_SELF_DIRECTED: "plannedSelfDirected",
+	GROUP: 'group',
+	ANNOUNCEMENT: 'announcement',
+	ONE_ON_ONE: '1on1',
+	SELF_DIRECTED: 'selfDirected',
+	PLANNED_ONE_ON_ONE: 'planned1on1',
+	PLANNED_SELF_DIRECTED: 'plannedSelfDirected'
 } as const;
 
-export type IndividualProgram = unknown;
+type EnumValues = typeof programModeEnumMap;
+export type AllProgram = EnumValues[keyof EnumValues];
+
+export type IndividualProgram = Exclude<AllProgram, 'group' | 'announcement'>;
 
 type tests = [
-  Expect<
-    Equal<
-      IndividualProgram,
-      "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
-    >
-  >,
+	Expect<
+		Equal<
+			IndividualProgram,
+			'1on1' | 'selfDirected' | 'planned1on1' | 'plannedSelfDirected'
+		>
+	>
 ];
