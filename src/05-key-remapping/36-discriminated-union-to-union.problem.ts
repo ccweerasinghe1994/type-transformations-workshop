735303cec4898646d29b0ba9204eac83aee84c32
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-17 17:05:01 +0530

    Update 36-discriminated-union-to-union.problem.ts

--- src/05-key-remapping/36-discriminated-union-to-union.problem.ts
+++ src/05-key-remapping/36-discriminated-union-to-union.problem.ts
@@ -1,23 +1,27 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 type Fruit =
-  | {
-      name: "apple";
-      color: "red";
-    }
-  | {
-      name: "banana";
-      color: "yellow";
-    }
-  | {
-      name: "orange";
-      color: "orange";
-    };
+	| {
+			name: 'apple';
+			color: 'red';
+	  }
+	| {
+			name: 'banana';
+			color: 'yellow';
+	  }
+	| {
+			name: 'orange';
+			color: 'orange';
+	  };
 
-type TransformedFruit = unknown;
+type TransformedFruit = {
+	[R in Fruit as R['name']]: `${R['name']}:${R['color']}`;
+}[Fruit['name']];
+
+type test = TransformedFruit;
 
 type tests = [
-  Expect<
-    Equal<TransformedFruit, "apple:red" | "banana:yellow" | "orange:orange">
-  >,
+	Expect<
+		Equal<TransformedFruit, 'apple:red' | 'banana:yellow' | 'orange:orange'>
+	>
 ];
