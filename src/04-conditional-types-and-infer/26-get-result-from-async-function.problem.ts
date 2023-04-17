0e8c4c26d5f6dd3e23c7a2e9f8fdadb2f736fcb3
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-17 09:51:28 +0530

    Update 26-get-result-from-async-function.problem.ts

--- src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
+++ src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
@@ -1,22 +1,30 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 const getServerSideProps = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const json: { title: string } = await data.json();
-  return {
-    props: {
-      json,
-    },
-  };
+	const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+	const json: { title: string } = await data.json();
+	return {
+		props: {
+			json
+		}
+	};
 };
 
-type InferPropsFromServerSideFunction = unknown;
+type InferPropsFromServerSideFunction<T> = T extends (
+	...args: any
+) => Promise<infer ReturnType>
+	? ReturnType extends { ['props']: infer PropsType }
+		? PropsType
+		: never
+	: never;
+
+type test = InferPropsFromServerSideFunction<typeof getServerSideProps>;
 
 type tests = [
-  Expect<
-    Equal<
-      InferPropsFromServerSideFunction<typeof getServerSideProps>,
-      { json: { title: string } }
-    >
-  >
+	Expect<
+		Equal<
+			InferPropsFromServerSideFunction<typeof getServerSideProps>,
+			{ json: { title: string } }
+		>
+	>
 ];
